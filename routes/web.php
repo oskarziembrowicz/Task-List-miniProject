21eb72e37d700afeb997f3440dfdf1318a0dc2a4
oskarziembrowicz <rakson2002@gmail.com>
2025-04-05 13:33:32 +0200

    Adding basic blade template

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Main page";
+    return view("index", [
+        "name" => "Oskar"
+    ]);
 });
 
 Route::get("/hello", function () {
