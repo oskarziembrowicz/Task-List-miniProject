f33af757b0244affcd491dd6c380db2fc2d6d7f6
oskarziembrowicz <rakson2002@gmail.com>
2025-04-05 13:15:48 +0200

    Playing around with routing

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,16 @@ Route::get('/', function () {
 
 Route::get("/hello", function () {
     return "Hello";
+})->name("hello");
+
+Route::get("/hallo", function () {
+    return redirect()->route("hello");
 });
 
 Route::get("/greet/{name}", function ($name) {
     return "Hello " . $name . "!";
+});
+
+Route::fallback(function () {
+    return "Still got somewhere!";
 });
